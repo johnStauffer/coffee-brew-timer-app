b7e21ab4aef1a97e82b6618ac8823b665c501214
John Stauffer <jstauffer@homeadvisor.com>
2016-07-13 12:16:45 -0600

    Splash Screen

---
+++ app/src/main/java/com/example/jstauffer/bloomcoffeeassistant/Splash.java
@@ -0,0 +1,24 @@
+package com.example.jstauffer.bloomcoffeeassistant;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                    //TODO make intent
+                        Intent intent = new Intent(Splash.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }, 2000
+        );
+    }
+}
