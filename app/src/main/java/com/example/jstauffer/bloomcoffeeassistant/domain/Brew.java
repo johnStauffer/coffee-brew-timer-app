3901bd4959c54c9b1d041108ae2007313007a068
John Stauffer <jstauffer@homeadvisor.com>
2016-07-19 14:31:09 -0600

    Brew domain objects

--- app/src/main/java/com/example/jstauffer/bloomcoffeeassistant/domain/Brew.java
+++ app/src/main/java/com/example/jstauffer/bloomcoffeeassistant/domain/Brew.java
@@ -4,4 +4,45 @@ package com.example.jstauffer.bloomcoffeeassistant.domain;
  * Created by jstauffer on 7/15/16.
  */
 public class Brew {
+    private int grindMass;
+    private int waterMass;
+    private int brewTime;
+    private int numCups;
+
+
+
+    public int getGrindMass() {
+        return grindMass;
+    }
+
+    public void setGrindMass(int grindMass) {
+        this.grindMass = grindMass;
+    }
+
+    public int getWaterMass() {
+        return waterMass;
+    }
+
+    public void setWaterMass(int waterMass) {
+        this.waterMass = waterMass;
+    }
+
+    public int getBrewTime() {
+        return brewTime;
+    }
+
+    public void setBrewTime(int brewTime) {
+        this.brewTime = brewTime;
+    }
+
+    public int getNumCups() {
+        return numCups;
+    }
+
+    public void setNumCups(int numCups) {
+        this.numCups = numCups;
+    }
+
+    private enum brewSystem {Aeropress, Chemex, Hario}
+
 }
