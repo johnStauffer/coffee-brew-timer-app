f536b586124d389d0550e1a1e4b33f1c8d9536c3
John Stauffer <jstauffer@homeadvisor.com>
2016-12-02 15:37:58 -0700

    Brew animation

---
+++ app/src/main/java/com/example/jstauffer/bloomcoffeeassistant/ui/BrewActivity.java
@@ -0,0 +1,40 @@
+package com.example.jstauffer.bloomcoffeeassistant.ui;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jstauffer.bloomcoffeeassistant.R;
+import com.mikhaellopez.circularfillableloaders.CircularFillableLoaders;
+
+public class BrewActivity extends AppCompatActivity {
+
+    CountDownTimer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brew);
+        startCfp();
+        timer.start();
+    }
+
+    private void startCfp(){
+        final CircularFillableLoaders cfp = (CircularFillableLoaders) findViewById(R.id.circularFillableProgress);
+        cfp.setProgress(0);
+        timer = new CountDownTimer(50000, 500) {
+            int i = 100;
+            @Override
+            public void onTick(long millisUntilFinished) {
+                cfp.setProgress(i);
+                i--;
+                System.out.println("Time: "+i);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+    }
+}
--- app/src/main/java/com/example/jstauffer/bloomcoffeeassistant/ui/MassActivity.java
+++ app/src/main/java/com/example/jstauffer/bloomcoffeeassistant/ui/MassActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.jstauffer.bloomcoffeeassistant.R;
 import com.example.jstauffer.bloomcoffeeassistant.domain.BrewSystem;
@@ -40,26 +39,29 @@ public class MassActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startBrew();
             }
         });
     }
 
-    private Toolbar initToolbar (){
+    private Toolbar initToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //Capitalize brew method for display
-        displayMethod = brewMethod.substring(0,1).toUpperCase()+brewMethod.substring(1);
+        displayMethod = brewMethod.substring(0, 1).toUpperCase() + brewMethod.substring(1);
         //Set toolbar title
         toolbar.setTitle(String.format("%s Grind Mass", displayMethod));
 
         return toolbar;
     }
 
-    private void initWithIntent(Intent intent){
+    private void initWithIntent(Intent intent) {
         brewMethod = (String) intent.getSerializableExtra("brew_method");
         brewSystem = brewFactory.getBrewSystem(brewMethod);
+    }
 
+    private void startBrew(){
+        Intent intent = new Intent(this, BrewActivity.class);
+        startActivity(intent);
     }
 
 }
